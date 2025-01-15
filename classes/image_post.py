c06c2fa10b44a576e5968c25ab17016e311a2a89
cfir <cfir0910@gmail.com>
2025-01-15 17:12:53 +0200

    started image post

--- classes/image_post.py
+++ classes/image_post.py
@@ -1,10 +1,15 @@
 import pygame
 from classes import Post
 from constants import *
+from helpers import *
 
 
 class ImagePost(Post):
-    def __init__(self, username, location, description):
+    def __init__(self, username, location, description, image):
         super().__init__(self, username, location, description)
+        self.image = image
+
+    def display(self):
         self.image = pygame.image.load("Images/ronaldo.jpg")
         pygame.transform.scale(self.image, (POST_WIDTH, POST_HEIGHT))
+        screen.blit(self.image, (POST_X_POS, POST_Y_POS))
