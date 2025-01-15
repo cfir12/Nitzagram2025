c6ce795110d219efe550dfd9edb07ad88028d4d0
cfir <cfir0910@gmail.com>
2025-01-15 17:51:48 +0200

    imagepost

--- classes/image_post.py
+++ classes/image_post.py
@@ -5,11 +5,12 @@ from helpers import *
 
 
 class ImagePost(Post):
-    def __init__(self, username, location, description, image):
+    def __init__(self, username, location, description, src):
         super().__init__(self, username, location, description)
-        self.image = image
+        self.src = src
 
     def display(self):
-        self.image = pygame.image.load("Images/ronaldo.jpg")
-        pygame.transform.scale(self.image, (POST_WIDTH, POST_HEIGHT))
-        screen.blit(self.image, (POST_X_POS, POST_Y_POS))
+        image = pygame.image.load(self.src)
+        pygame.transform.scale(image, (POST_WIDTH, POST_HEIGHT))
+        screen.blit(image, (POST_X_POS, POST_Y_POS))
+        super().display()
